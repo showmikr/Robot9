3f96463b80ae138e6eea04cb4543470b8f22ff94
Richard Corn <crossle86@users.noreply.github.com>
2015-12-07 17:10:08 -0800

    Fix Util.currentMethod to use literal "Robot9" instead of "Robot8".

--- src/Team4450/Lib/Util.java
+++ src/Team4450/Lib/Util.java
@@ -208,7 +208,7 @@ public class Util
     
         try
         {
-        	return stackTrace[level].toString().split("Robot8.")[1];
+        	return stackTrace[level].toString().split("Robot9.")[1];
         }
         catch (Throwable e)
         {
